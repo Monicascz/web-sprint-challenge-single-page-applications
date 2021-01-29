39c151178af548896312d2e52d095a9b2c4ae64b
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-01-29 07:57:58 -0800

    Radio buttons set and working properly.

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 export default function Form(props) {
     const { values, change } = props
+    console.log("sauce1 ", values.sauce)
     const onchange = (evt) => {
         const { name, value} = evt.target;
         change(name, value);
-
+        
     }
     
     return (
@@ -15,6 +16,7 @@ export default function Form(props) {
             <p> [Insert Image Here]</p>
             <h4> Build Your Own Pizza</h4>
             <form>
+                {/* //////// SIZE DROPDOWN /////////// */}
                 <label>
                     Choice of Size (required)
                     <br></br>
@@ -24,6 +26,37 @@ export default function Form(props) {
                         <option value='med'>Medium</option>
                         <option value='large'>Large</option>
                     </select>
+                </label>
+                <br></br>
+                <br></br>
+                <br></br>
+                {/* //////// SAUCE RADIO BUTTONS  /////////// */}
+                <p>Choice of Sauce (required)</p>
+                <label> 
+                    <input name="sauce" type='radio' value='ogred' checked={values.sauce === 'ogred'} onChange={onchange}>
+                    </input>
+                   Original Red: 
+                </label>
+                <br></br>
+                <label> 
+                    <br></br>
+                    <input name="sauce" type='radio' value='garlic' checked={values.sauce === 'garlic'} onChange={onchange}>
+                    </input>
+                   Garlic Ranch: 
+                </label>
+                <br></br>
+                <label> 
+                    <br></br>
+                    <input name="sauce" type='radio' value='bbq' checked={values.sauce === 'bbq'} onChange={onchange}>
+                    </input>
+                  BBQ Sauce: 
+                </label>
+                <br></br>
+                <label> 
+                    <br></br>
+                    <input name="sauce" type='radio' value='spinach' checked={values.sauce === 'spinach'} onChange={onchange}>
+                    </input>
+                   Spinach Alfredo: 
                 </label>
             </form>
         </div>
